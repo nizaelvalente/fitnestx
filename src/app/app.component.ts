78c1fcca66d18f442a9e5e5124ea44d1b613c212
erickleite <erick.leite@fcamara.com.br>
2021-10-13 22:49:40 -0300

    merge with component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fitnestx';
+
+  public show() {
+    alert('Clicou!');
+  }
+
+  public mostrarResultado(dados: any) {
+    console.log(dados);
+  }
 }
